b1627d9083560c704ff56f4252a625406b8f07f1
Ahmed Mulla <ahmedmulla1910@gmail.com>
2023-02-05 12:09:08 +0530

    Changes

--- face_detection/settings.py
+++ face_detection/settings.py
@@ -124,3 +124,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, ''),
+    )
